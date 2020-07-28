39588147e6ca5a955e9faba981d6094c59fa15a7
EmilMachine <emil.lime@gmail.com>
2020-07-28 18:18:24 +0200

    model api work locally

--- 08_docker_deploy_ml/app/main.py
+++ 08_docker_deploy_ml/app/main.py
@@ -4,77 +4,84 @@ import os
 from typing import List, Optional
 
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+
 import numpy as np
 import pandas as pd
+import xgboost
+import joblib
 from pydantic import BaseModel
+
 
 import uvicorn
 
-logger = logging.getLogger("model_deaths")
+logger = logging.getLogger("model_titanic_survival")
 
 v1_path = "/v1"
-model_path = "/deaths/confirmed"
+model_path = "/titanic"
+
+#/v1/titanic/single/{country}
 
 logger.info("Loading API")
 app = FastAPI(
-    title="COVID-19 Deaths Prediction",
-    description="This API exposes predictions of the number of deaths.",
-    version="0.1.1",
+    title="Titanic survival",
+    description="This API exposes predictions for survival of the Titanic disaster.",
+    version="0.1.0",
     openapi_url=f"{v1_path}{model_path}/openapi.json",
     docs_url=f"{v1_path}{model_path}/docs",
     redoc_url=f"{v1_path}{model_path}/redoc",
 )
 
-predictions_path = os.getenv("PREDICTIONS_PATH", "app/predictions.pkl")
+
+predictions_path = os.getenv("PREDICTIONS_PATH", "data/model_xgboost_best.sav")
 logger.info(f"Loading predictions from {predictions_path}")
-predictions = pd.read_pickle(predictions_path)
+loaded_model = joblib.load(predictions_path)
 
 
-class Prediction(BaseModel):
-    timestamp: datetime
-    deaths_prediction: np.float64
-    cumulative_deaths_prediction: np.float64
-    residuals_low: np.float64
-    residuals_high: np.float64
-    days_since_100_cases: int
-    deaths: Optional[float]
-    cumulative_deaths: Optional[float]
-
-    def __init__(self, **data) -> None:
-        """Custom init to parse datetime64"""
-        if isinstance(data["timestamp"], pd.Timestamp):
-            data["timestamp"] = data["timestamp"].to_pydatetime()
-        super().__init__(**data)
+def get_prediction(x):
+    y = loaded_model.predict(x)[0]  # just get single value
+    prob = loaded_model.predict_proba(x)[0].tolist()  # send to list for return
+    return {'prediction': int(y), 'probability': prob}
 
 
-class PredictionsOut(BaseModel):
-    country: str
-    result: List[Prediction]
+class ModelParams(BaseModel):
+    PassengerId   : np.int
+    Pclass        : np.int
+    Sex           : np.int
+    Age           : np.float
+    SibSp         : np.int
+    Parch         : np.int
+    Fare          : np.float
+    FamilySize    : np.int
+    Has_Cabin     : np.int
+    Name_length   : np.int
+    Ticket_length : np.int
+    Embarked_Q    : np.int
+    Embarked_S    : np.int
+    Embarked_C    : np.int
 
 
-@app.get(v1_path + model_path + "/countries/{country}")
-def read_root(
-    country: str,
-    time_from: datetime = datetime(2019, 1, 1),
-    time_to: datetime = datetime(2222, 1, 1),
-) -> PredictionsOut:
-    df = predictions[predictions["country"] == country]
-    df = df[(df.index >= time_from) & (df.index < time_to)]
-    df = df.where(pd.notnull(df), None)  # Make nans a None so that it encodes properly
-    return PredictionsOut(
-        country=country,
-        result=list(map(lambda x: Prediction(**x), df.to_dict(orient="records"))),
-    )
+@app.post(f"{v1_path}{model_path}/predict")
+def predict(params: ModelParams):
+
+    #pd.Dataframe([model.dict() for model in data])
+
+    df = pd.DataFrame(params.dict(), index=[1])
+    pred = get_prediction(df)
+    return pred
+
+# Official documentation for fastapi
+# https://fastapi.tiangolo.com/tutorial/path-params/
 
 
-@app.get(f"{v1_path}{model_path}/countries")
-def read_item():
-    return predictions["country"].unique().tolist()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
-@app.get(f"{v1_path}{model_path}/health")
-def redirect():
-    return {"detail": "ok"}
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
 
 
 if __name__ == "__main__":
